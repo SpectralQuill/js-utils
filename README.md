617c98fe1504a19fe147641899da48c6e5467802
Gian Tristian G. Apostol <giantristianapostol@gmail.com>
2023-08-05 19:09:40 +0800

    Added new interfaces

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Description
 Additional methods for existing JavaScript objects.
 
+Objects affected:
+    1. Array prototype
+
 # How to Edit
 1. Edit "src/main.ts"
-2. Type in command "npx tsc src/index.ts" to create a new "index.js"
-3. Replace "index.js" in root directory with new one
+2. Type in command "npx tsc src/index.ts" within terminal to create a new "index.js"
+3. Replace "index.js" from root directory with new one
+
+# Don'ts
+1. Do not create import statements within "index.ts"
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,85 @@
+type addend = number;
+type amt = number;
+type collection = element[] | Set<element>;
+type count = number;
+type element = any;
 type index = number;
+type indexCollection = index[] | Set<index>;
+type int = number;
 type possibleIndex = index | undefined;
+type possibleInfiniteAmt = number | true;
+type setGeneralBooleanCallbackFn = (element: element, set: Set<element>) => boolean;
+
+// bookmark: collection
+
+interface Collection<T> {
+
+    isEmpty(): boolean;
+
+}
 
 // bookmark: array
 
-interface Array<T> {
+interface Array<T> extends Collection<T> {
 
-    indexes(): index[];
-    lastIndex(): possibleIndex;
+    addElements(elements: collection, multiplier: int): index[]; //
+    count(elements: collection): amt; //
+    clear(): element[]; //
+    deleteElements(
+        elementsOrCallbackFn:
+            collection | ((
+                element: element,
+                index: index,
+                array: this,
+                deleted: element[]
+            ) => boolean)
+        ,
+        amt: possibleInfiniteAmt,
+        start: index,
+        end: index
+    ): element[]; //
+    firstElement(): element; //
+    firstIndex(positive: boolean): possibleIndex; //
+    indexes(positive: boolean): index[]; //
+    indexOfInt(positive: boolean): index; //
+    largest(exclude: collection): element; //
+    lastElement(): element; //
+    lastIndex(positive: boolean): possibleIndex; //
+    middleElement(): element; //
+    middleIndex(positive: boolean): possibleIndex; //
+    multiply(multiplier: int): index[]; //
+    randomElement(exclude: collection): element; //
+    randomIndex(exclude: indexCollection, positive: boolean): index; //
+    smallest(exclude: collection): element; //
 
 }
 
-Array.prototype.indexes = function() {
+Array.prototype.isEmpty = function() {
 
-    return this.map((_: any, index: string) => index);
+    return this.length == 0;
 
 }
 
-Array.prototype.lastIndex = function() {
+// bookmark: set
 
-    const { length } = this;
-    return length > 0 ? length - 1 : undefined;
+interface Set<T> extends Collection<T> {
+
+    addElements(elements: collection): Set<element>; //
+    deleteElements(
+        elementsOrCallbackFn:
+            collection | ((element: element, set: this, deleted: Set<element>) => boolean)
+    ): Set<element>; //
+    difference(set: collection): Set<element>; //
+    every(callbackFn: setGeneralBooleanCallbackFn, thisArg: any): boolean; //
+    filter(callbackFn: setGeneralBooleanCallbackFn, thisArg: any): Set<element>; //
+    intersection(set: collection): Set<element>; //
+    some(callbackFn: setGeneralBooleanCallbackFn, thisArg: any): boolean; //
+    union(set: collection): Set<element> //
+
+}
+
+Set.prototype.isEmpty = function() {
+
+    return this.size == 0;
 
 }
