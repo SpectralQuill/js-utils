90ade829fe57bdf6c0b8acc3d6126728c3f29246
Gian Tristian G. Apostol <giantristianapostol@gmail.com>
2023-08-30 16:25:35 +0800

    updated ArrayUtils.count()

--- src/array.ts
+++ src/array.ts
@@ -7,7 +7,7 @@ import Range from "./range";
 module ArrayUtils {
 
     export type callback<T, R> = (element: T, index: index, array: T[]) => R;
-    export type match<T> = T | ArrayUtils.callback<T, boolean>;
+    export type match<T> = ArrayUtils.callback<T, boolean>;
 
 }
 
@@ -15,14 +15,6 @@ export default class ArrayUtils {
 
     // count, delete, indexes (start)
     // multiply should take decimals as well
-
-    private static makeMatchFn<T>(
-        match: ArrayUtils.match<T>
-    ): ArrayUtils.callback<T, boolean> {
-
-        return match instanceof Function ? match : (element => element == match);
-
-    }
 
     // public static count<T>(array: T[], match: ArrayUtils.match<T>): number {
 
@@ -44,17 +36,17 @@ export default class ArrayUtils {
         const forward: boolean = !NumberUtils.isNegative(start);
         const period: int = forward ? 1 : -1;
         const negativeFirstIndex: index = this.negativeIndexOfInt(array, 0) as number;
+        console.log(negativeFirstIndex);
         const condition: (value: number) => boolean =
             forward ? (value => value < length) :
             (value => value < negativeFirstIndex)
         ;
         const iterator: Counter = new Counter(start, period, condition);
-        const check: ArrayUtils.callback<T, boolean> = ArrayUtils.makeMatchFn<T>(match);
         let count: int = 0;
         for(let index of iterator) {
 
             const element: T = array.at(index) as T;
-            if(check(element, index, array)) count++;
+            if(match(element, index, array)) count++;
 
         };
         return count;
@@ -63,12 +55,11 @@ export default class ArrayUtils {
 
     public static delete<T>(array: T[], match: ArrayUtils.match<T>): T[] {
 
-        const check: ArrayUtils.match<T> = ArrayUtils.makeMatchFn<T>(match);
         const deleted: T[] = [];
         for(let index: index = 0; index < array.length;) {
 
             const element = array[index];
-            if(check(element, index, array)) {
+            if(match(element, index, array)) {
 
                 array.splice(index, 1);
                 deleted.push(element);
