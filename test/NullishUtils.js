613e5d3746086df5194ec0793124f72e7acb3ff6
SpectralQuill <89404207+SpectralQuill@users.noreply.github.com>
2023-12-15 17:05:35 +0800

    gitignore applied

--- test/NullishUtils.js
+++
@@ -1,5 +0,0 @@
-export default class NullishUtils {
-    static makeUndefined() {
-        return undefined;
-    }
-}
--- test/test.js
+++
@@ -1,6 +0,0 @@
-import Range from "./Range.js";
-const range = new Range(2, 8, false, true, -3);
-for (let i = 0; i < 10; i++) {
-    console.log(range.pickNumber());
-}
-// console.log( range.inRange( 2))
